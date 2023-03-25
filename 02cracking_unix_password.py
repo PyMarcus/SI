ee7799cc08eb1d78fad9a79f743b6634abd5ae8c
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-03-25 18:09:49 -0300

    02

---
+++ 02cracking_unix_password.py
@@ -0,0 +1,30 @@
+import crypt
+
+PATH: str = "/etc/shadow"
+SALT: str = "$6$"
+
+
+def hash_it(word: str, salt: str) -> str:
+    return crypt.crypt(word, salt)
+
+
+def password():
+    global SALT, PATH
+    with open(PATH, 'r') as shadow:
+        for line in shadow.readlines():
+            word = line.split('$')
+            if len(word) > 2:
+                SALT = f"${word[1]}${word[2]}${word[3]}$".strip()
+                return line.split(':')[1]
+        return ""
+
+
+def read_dict_file(filename: str) -> str or None:
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            if hash_it(line, SALT) == password():
+                return line
+
+
+if __name__ == '__main__':
+    print(read_dict_file('teste.txt'))
